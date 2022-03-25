bf34b64c2e7b967c8b2134957ff6c118228cd97c
ghazaliiii <albanifaishal5@gmail.com>
2022-03-25 16:29:51 +0700

    first commit

---
+++ src/tpmod5_13022200103/Tpmod5_13022200103.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpmod5_13022200103;
+
+/**
+ *
+ * @author faishal
+ */
+public class Tpmod5_13022200103 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
